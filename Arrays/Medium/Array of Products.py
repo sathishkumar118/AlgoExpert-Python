087a54b2d334400ddaa2d09c018177753c2632d5
Sathish Kumar S <ssathishkumar.118@gmail.com>
2022-10-18 20:13:22 +0530

    Better time complexity O(N)

--- Arrays/Medium/Array of Products.py
+++ Arrays/Medium/Array of Products.py
@@ -1,13 +1,17 @@
 """A Method that takes in an array, and for every element in the array, it calculates the product of all other elements excluding the selected one, thereby returns the products in the form of an array"""
 def arrayOfProducts(array):
     # Write your code here.
-    pdt = 1
-    arr_pdts = []
+    left,right = [1]*len(array),[1]*len(array)
+    res = []
+    product = 1
     for i in range(len(array)):
-        for j in range(len(array)):
-            if i != j:
-                pdt *= array[j]
-        arr_pdts.append(pdt)
-        pdt = 1
-    return arr_pdts
+        left[i] = product
+        product *= array[i]
+    product = 1
+    for i in range(len(array)-1,-1,-1):
+        right[i] = product
+        product *= array[i]
+    for i in range(len(array)):
+        res.append(left[i]*right[i])
+    return res
     pass
