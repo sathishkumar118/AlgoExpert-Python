9dec59a3847516db0d5df46f2eb595e9b6b17448
Sathish Kumar <ssathishkumar.118@gmail.com>
2022-11-13 20:05:14 +0530

    final solution

--- Binary Search Trees/Medium/BST Validation.py
+++ Binary Search Trees/Medium/BST Validation.py
@@ -1,3 +1,38 @@
-"""
-Validate the given BST tree
-"""
+# This is an input class. Do not edit.
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+def validateBst(tree):
+    # Write your code here.
+    if tree != None:
+        if tree.left == None and tree.right == None:
+            return True
+        elif tree.left == None:
+            if tree.value <= findMin(tree.right):
+                return validateBst(tree.right)
+            else:
+                return False
+        elif tree.right == None:
+            if tree.value > findMax(tree.left):
+                return validateBst(tree.left)
+            else:
+                return False
+        else:
+            if tree.value > findMax(tree.left) and tree.value <= findMin(tree.right):
+                return validateBst(tree.left) and validateBst(tree.right)
+            else:
+                return False
+def findMax(tree):
+    if tree.right == None:
+        return tree.value
+    else:
+        return max(tree.value,findMax(tree.right))
+def findMin(tree):
+    if tree.left == None:
+        return tree.value
+    else:
+        return min(tree.value,findMin(tree.left))
