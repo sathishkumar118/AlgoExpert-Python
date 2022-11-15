4877c49e8d1483a2fbc1de895a4cffb31734c583
Sathish Kumar <ssathishkumar.118@gmail.com>
2022-11-15 22:03:27 +0530

    init commit; all test cases passed

---
+++ Binary Trees/Medium/Invert Binary Tree.py
@@ -0,0 +1,19 @@
+def invertBinaryTree(tree):
+    # Write your code here.
+    if tree == None:
+        return tree
+    else:
+        leftSubTree = invertBinaryTree(tree.left)
+        rightSubTree = invertBinaryTree(tree.right)
+        tree.left = rightSubTree
+        tree.right = leftSubTree
+        return tree
+    pass
+
+
+# This is the class of the input binary tree.
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
