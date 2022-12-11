f9ba2f6e0c520c9dad3da3b1b4f788169c9b412e
Sathish Kumar S <ssathishkumar.118@gmail.com>
2022-12-11 15:29:09 +0530

    5 test cases passed

--- Binary Search Trees/Medium/Min Height BST.py
+++ Binary Search Trees/Medium/Min Height BST.py
@@ -1,4 +1,30 @@
-"""
-Given an array, Construct a BST with minimum possible height.
-"""
+def minHeightBst(array, root = None):
+    root = BST(array.pop(int(len(array)/2)))
+    while len(array) > 0:
+        root.insert(array.pop(int(len(array)/2)))
+    return root
+    pass
 
+"""def findDepth(BST):
+    if BST == None:
+        return 0
+    return 1 + max(findDepth(BST.left),findDepth(BST.right))"""
+
+
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        if value < self.value:
+            if self.left is None:
+                self.left = BST(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right is None:
+                self.right = BST(value)
+            else:
+                self.right.insert(value)
