7abd83a18d97f5abfbe570362168bee65806a9c5
Sathish Kumar S <ssathishkumar.118@gmail.com>
2022-12-11 22:29:48 +0530

    remove unwanted lines

--- Binary Search Trees/Medium/reconstruct BST.py
+++ Binary Search Trees/Medium/reconstruct BST.py
@@ -1,10 +1,8 @@
-# This is an input class. Do not edit.
 class BST:
     def __init__(self, value, left=None, right=None):
         self.value = value
         self.left = left
         self.right = right
-
 
 def reconstructBst(preOrderTraversalValues):
     if preOrderTraversalValues == []:
