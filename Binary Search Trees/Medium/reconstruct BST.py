21bda6bebe556e0062456f74d194b98fde9a0c87
Sathish Kumar S <ssathishkumar.118@gmail.com>
2022-12-11 22:28:45 +0530

    first commit; all test cases passed

---
+++ Binary Search Trees/Medium/reconstruct BST.py
@@ -0,0 +1,24 @@
+# This is an input class. Do not edit.
+class BST:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+
+def reconstructBst(preOrderTraversalValues):
+    if preOrderTraversalValues == []:
+        return None
+    rootValue = preOrderTraversalValues.pop(0)
+    root = BST(rootValue)
+    if preOrderTraversalValues == []:
+        return root
+    leftArr = []
+    while preOrderTraversalValues[0] < rootValue:
+        leftArr.append(preOrderTraversalValues.pop(0))
+        if preOrderTraversalValues == []:
+            break
+    rightArr = preOrderTraversalValues
+    root.left = reconstructBst(leftArr)
+    root.right = reconstructBst(rightArr)
+    return root
