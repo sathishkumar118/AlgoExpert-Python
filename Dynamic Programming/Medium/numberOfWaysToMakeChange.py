54d4ada8c4b3a66ee0bf683f800c22e293760700
Sathish Kumar S <ssathishkumar.118@gmail.com>
2022-12-27 21:48:20 +0530

    brute force approach

--- Dynamic Programming/Medium/numberOfWaysToMakeChange.py
+++ Dynamic Programming/Medium/numberOfWaysToMakeChange.py
@@ -1,9 +1,20 @@
-def numberOfWaysToMakeChange(n, denoms):
+def numberOfWaysToMakeChange(n, denoms, child = False):
     if n == 0:
         return 1
-    denoms.sort()
     count = 0
+    combs = []
     for denom in denoms:
-        if n >= denom:
-            count += numberOfWaysToMakeChange(n - denom, denoms)
-    return count
+        if n == denom:
+            combs.append([denom])
+        elif n > denom:
+            lst = numberOfWaysToMakeChange(n - denom, denoms, True)
+            for elem in lst:
+                elem.append(denom)
+                elem.sort()
+                if elem not in combs:
+                    combs.append(elem)
+    print(combs)
+    if child:
+        return combs
+    else:
+        return len(combs)
