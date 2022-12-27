5225e8c807dde01b659df64abdddbafb71ff4f68
Sathish Kumar S <ssathishkumar.118@gmail.com>
2022-12-27 07:11:00 +0530

    initial commit

--- Dynamic Programming/Medium/numberOfWaysToMakeChange.py
+++ Dynamic Programming/Medium/numberOfWaysToMakeChange.py
@@ -1,4 +1,9 @@
 def numberOfWaysToMakeChange(n, denoms):
-    # Write your code here.
-    
-    pass
+    if n == 0:
+        return 1
+    denoms.sort()
+    count = 0
+    for denom in denoms:
+        if n >= denom:
+            count += numberOfWaysToMakeChange(n - denom, denoms)
+    return count
