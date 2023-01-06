812931f574a826d443d028310624519ff115d810
Sathish Kumar S <ssathishkumar.118@gmail.com>
2023-01-06 23:09:23 +0530

    try using DP

--- Dynamic Programming/Medium/levenshteinDistance.py
+++ Dynamic Programming/Medium/levenshteinDistance.py
@@ -1,16 +1,10 @@
 def levenshteinDistance(str1, str2):
-    l1 = len(str1)
-    l2 = len(str2)
-    if l1 > l2:
-        temp = str1[:]
-        flag = True
-        while temp == str2:
-            if temp == '':
-                return 2*l2-l1
-            if flag:
-                temp = temp[1:]
-                flag = False
-            else:
-                temp = temp[:-1]
-        return l1 - len(temp)
-    pass
+    l1, l2 = len(str1), len(str2)
+    arr = [[i] + [0] * l1 for i in range(l1 + 1)]
+    arr[0] = [i for i in range(l2 + 1)]
+    for i in range(1, l1 + 1):
+        for j in range(1, l2 + 1):
+            arr[i][j] = min(arr[i - 1][j], arr[i - 1][j - 1], arr[i][j - 1])
+            if str1[i - 1] != str2[j - 1]:
+                arr[i][j] += 1
+    return arr[-1][-1]
