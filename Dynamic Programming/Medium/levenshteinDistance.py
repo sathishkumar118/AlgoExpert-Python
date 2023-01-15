6c5e85254ee5a3b0f81bacde5032e4c7770869ee
Sathish Kumar S <ssathishkumar.118@gmail.com>
2023-01-15 11:34:45 +0530

    all test cases passes

--- Dynamic Programming/Medium/levenshteinDistance.py
+++ Dynamic Programming/Medium/levenshteinDistance.py
@@ -1,10 +1,10 @@
 def levenshteinDistance(str1, str2):
     l1, l2 = len(str1), len(str2)
-    arr = [[i] + [0] * l1 for i in range(l1 + 1)]
+    arr = [[i] + [0] * l2 for i in range(l1 + 1)]
     arr[0] = [i for i in range(l2 + 1)]
     for i in range(1, l1 + 1):
         for j in range(1, l2 + 1):
-            arr[i][j] = min(arr[i - 1][j], arr[i - 1][j - 1], arr[i][j - 1])
-            if str1[i - 1] != str2[j - 1]:
-                arr[i][j] += 1
+            arr[i][j] = min(arr[i - 1][j], arr[i - 1][j - 1], arr[i][j - 1]) + 1
+            if str1[i - 1] == str2[j - 1]:
+                arr[i][j] = arr[i - 1][j - 1]
     return arr[-1][-1]
