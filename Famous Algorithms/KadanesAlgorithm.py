0d42caf13cef40a8fec3a099346b10760b414cb6
Sathish Kumar S <ssathishkumar.118@gmail.com>
2023-01-21 22:37:54 +0530

    all test cases passed

--- Famous Algorithms/KadanesAlgorithm.py
+++ Famous Algorithms/KadanesAlgorithm.py
@@ -1,10 +1,8 @@
 def kadanesAlgorithm(array):
     currSum = array[0]
     maxSum = currSum
-    for item in array:
-        currSum += item
-        if currSum < 0:
-            currSum = 0
+    for item in array[1:]:
+        currSum = max(currSum + item,item)
         if currSum > maxSum:
             maxSum = currSum
     return maxSum
