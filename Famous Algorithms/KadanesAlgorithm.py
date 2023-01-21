98a8ecf503fe29d3a4e718321392568b61cd5dc4
Sathish Kumar S <ssathishkumar.118@gmail.com>
2023-01-21 22:27:40 +0530

    first commit

---
+++ Famous Algorithms/KadanesAlgorithm.py
@@ -0,0 +1,10 @@
+def kadanesAlgorithm(array):
+    currSum = array[0]
+    maxSum = currSum
+    for item in array:
+        currSum += item
+        if currSum < 0:
+            currSum = 0
+        if currSum > maxSum:
+            maxSum = currSum
+    return maxSum
